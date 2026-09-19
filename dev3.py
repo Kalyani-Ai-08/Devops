ca38c23f5131705c1d94e565f09ead4d9ec95a95
Ubuntu <ubuntu@ip-172-31-8-156.ap-southeast-1.compute.internal>
2026-09-19 01:22:26 +0000

    done by me

---
+++ dev3.py
@@ -0,0 +1,33 @@
+Stages of DevOps are:
+Plan Stage
+Defining project goals, requirements, and task breakdowns to align development and operations teams.
+
+Work is broken down into tasks and user stories to ensure clarity and alignment.
+Collaboration between development and operations begins at this stage to avoid future bottlenecks.
+Common Tools: Jira, Confluence, Azure Boards, Trello.
+Code Stage
+Writing, reviewing, and managing application source code and configurations using version control.
+
+Code reviews and branching strategies help maintain code quality and stability.
+Common Tools: Git, GitHub, GitLab, Bitbucket.
+Build Stage
+Automatically compiling and packaging code into deployable artifacts while resolving dependencies.
+
+Build automation ensures faster feedback and reduces manual errors.
+Common Tools: Jenkins, GitLab CI/CD, Maven, Gradle, Docker.
+Test Stage
+Running automated quality, security, and performance checks to identify bugs before release.
+
+Different Testing methods includes unit, integration, performance, and security testing.
+Issues are identified early, that will reduce the cost and impact of failures.
+Common Tools: Selenium, JUnit, TestNG, SonarQube, JMeter
+Release Stage
+Finalizing and documenting approved builds for deployment through version tagging and strategy planning.
+
+Deployment strategies are planned to minimize risk during production roll out.
+Common Tools: Git tags, Jenkins, GitLab CI/CD, ArgoCD.
+Deploy Stage
+Pushing the application into production environments using automated infrastructure and rollout strategies.
+
+Deployment strategies such as blue-green, canary, or rolling updates are used to ensure minimal downtime.
+Common Tools: Kubernetes, Helm, Ansible, Terraform.
